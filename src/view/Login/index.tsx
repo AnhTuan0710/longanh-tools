6c57995d2ebd5e62eca038a7b9934afb5621aca5
anhtuan@123 <eco0866_tuannv@MAC-27.local>
2022-11-21 10:22:48 +0700

    update router of app

--- src/view/Login/index.tsx
+++ src/view/Login/index.tsx
@@ -1,9 +1,13 @@
 import { Button } from 'antd';
+import { Link } from 'react-router-dom';
+import './login.scss'
 
 function Login() {
   return (
     <div>
       <Button>Login</Button>
+      <Link to='/'>Dashboard</Link>
+      <h2 className='test'>Moi ban dang nhap de su dung</h2>
     </div>
   );
 }
